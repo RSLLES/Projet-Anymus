bd1e12512e31caa843fc7c0b8735b2c67678b9b4
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-03 12:43:55 +0200

    clean up

--- Scripts/Get random faces.sh
+++ Scripts/Get random faces.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 ls
 echo $PWD
-dest="/d/HDD_Documents/Mines/Cours/1A/Projet_anime/2000Faces/"
+dest="/d/HDD_Documents/Mines/Cours/1A/Projet_anime/TrainFaces/"
 cd "105_classes_pins_dataset/"
 find . -type f | \
-    shuf -n 2000 | \
+    shuf -n 25000 | \
     while [ $(du -ks "$dest" | awk '{ print $1 }') -lt 10485760 ] && IFS= read -r fn; do
         cp "$fn" "$dest"
     done
---
+++ Scripts/recursive_image_extraction.bat
@@ -0,0 +1,3 @@
+md "D:\HDD_Documents\Mines\Cours\1A\Projet_anime\TrainFaces"
+cd /d "D:\HDD_Documents\Mines\Cours\1A\Projet_anime\105_classes_pins_dataset"
+for /r %d in (*) do copy "%d" "D:\HDD_Documents\Mines\Cours\1A\Projet_anime\TrainFaces"
