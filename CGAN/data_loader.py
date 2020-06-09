d8d06cfcdb0b5248aa0d719c4779ce5e1a2ffc65
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-09 15:05:02 +0200

    Remove spicy for keras

--- CGAN/data_loader.py
+++ CGAN/data_loader.py
@@ -1,5 +1,6 @@
-import scipy
 from glob import glob
+from keras_preprocessing.image import load_img
+from keras_preprocessing.image import img_to_array
 import numpy as np
 
 class DataLoader():
@@ -16,13 +17,8 @@ class DataLoader():
         imgs = []
         for img_path in batch_images:
             img = self.imread(img_path)
-            if not is_testing:
-                img = scipy.misc.imresize(img, self.img_res)
-
-                if np.random.random() > 0.5:
-                    img = np.fliplr(img)
-            else:
-                img = scipy.misc.imresize(img, self.img_res)
+            if not is_testing and np.random.random() > 0.5:
+                img = np.fliplr(img)
             imgs.append(img)
 
         imgs = np.array(imgs)/127.5 - 1.
@@ -50,9 +46,6 @@ class DataLoader():
                 img_A = self.imread(img_A)
                 img_B = self.imread(img_B)
 
-                img_A = scipy.misc.imresize(img_A, self.img_res)
-                img_B = scipy.misc.imresize(img_B, self.img_res)
-
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
                         img_B = np.fliplr(img_B)
@@ -67,9 +60,9 @@ class DataLoader():
 
     def load_img(self, path):
         img = self.imread(path)
-        img = scipy.misc.imresize(img, self.img_res)
         img = img/127.5 - 1.
         return img[np.newaxis, :, :, :]
 
     def imread(self, path):
-        return scipy.misc.imread(path, mode='RGB').astype(np.float)
+        pixels = load_img(path, target_size=self.img_res)
+        return img_to_array(pixels)
