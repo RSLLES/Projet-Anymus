d4f7e6dd35fe684b89c9ec29251302dca4f91b54
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-12 17:38:13 +0200

    dropout to 0.3

--- CGAN/cyclegan.py
+++ CGAN/cyclegan.py
@@ -210,7 +210,7 @@ def build_generator():
         d = InstanceNormalization()(d)
         return d
 
-    def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0.2):
+    def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0.3):
         """Layers used during upsampling"""
         u = UpSampling2D(size=2)(layer_input)
         u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
