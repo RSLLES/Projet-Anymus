fdf3d3ee7996e57d07014b17f6d93bfdd93e6772
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-16 22:39:05 +0200

    remove unused structure + update weights path

--- CGAN/cyclegan.py
+++ CGAN/cyclegan.py
@@ -22,18 +22,18 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 ### Constantes du programmes ###
 
 # Sur l'entrainement
-BATCH_SIZE = 5
+BATCH_SIZE = 3
 EPOCHS = 200
 SAMPLE_INTERVAL = 200
 
 # Input shape
-IMG_ROWS = 128
-IMG_COLS = 128
+IMG_ROWS = 1024
+IMG_COLS = 1024
 CHANNELS = 3
 IMG_SHAPE = (IMG_ROWS, IMG_COLS, CHANNELS)
 
 # Number of filters in the first layer of G and D
-GF, DF = 64, 32
+GF, DF = 32, 32
 
 # Loss weights
 LAMBDA_CYCLE = 10               # Cycle-consistency loss
@@ -41,7 +41,7 @@ LAMBDA_ID = 0.1 * LAMBDA_CYCLE    # Identity loss
 
 #Optimize
 learning_rate = 0.0002
-discr_factor = 0.25
+discr_factor = 0.3
 OPTIMIZER = Adam(learning_rate, 0.5)
 OPTIMIZER_D = Adam(learning_rate*discr_factor, 0.5)
 
@@ -52,42 +52,15 @@ OPTIMIZER_D = Adam(learning_rate*discr_factor, 0.5)
 ########################
 
 # Configure data loader
-dataset_name = 'face2manga'
+dataset_name = 'landscape2myazaki'
 data_loader = DataLoader(dataset_name=dataset_name, img_res=(IMG_ROWS, IMG_COLS))
-
-
 
 #
 # Construction des réseaux
 #
 
-def build_discriminator_improved(name=""):
-    def d_layer(layer_input, filters, f_size=4, normalization=True, drate=1):
-        d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
-        if drate > 1:
-            dprime = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
-            dprime = Conv2D(filters, kernel_size=f_size, dilation_rate=drate, padding='same')(dprime)
-            d = Concatenate()([d,dprime])
-        d = LeakyReLU(alpha=0.2)(d)
-        if normalization:
-            d = InstanceNormalization()(d)
-        return d
-
-    img = Input(shape=IMG_SHAPE)
-
-    d1 = d_layer(img, DF, drate=8, normalization=False)
-    d2 = d_layer(d1, DF*2, drate=4)
-    d3 = d_layer(d2, DF*4, drate=2)
-    d4 = d_layer(d3, DF*8)
-
-    validity = Conv2D(1, kernel_size=4, strides=1, padding='same')(d4)
-
-    model = Model(img, validity, name=name)
-    model.compile(loss='mse', optimizer=OPTIMIZER_D, metrics=['accuracy'])
-
-    return model
-
 def build_discriminator(name=""):
+    """ Format classique d'un discriminateur"""
 
     def d_layer(layer_input, filters, f_size=4, normalization=True):
         """Discriminator layer"""
@@ -110,99 +83,6 @@ def build_discriminator(name=""):
     model.compile(loss='mse', optimizer=OPTIMIZER_D, metrics=['accuracy'])
 
     return model
-
-def build_generator_improved():
-    def conv2d(layer_input, filters, s=2, f_size=4):
-        """Layers used during downsampling"""
-        d = Conv2D(filters, kernel_size=f_size, strides=s, padding='same')(layer_input)
-        d = LeakyReLU(alpha=0.2)(d)
-        d = InstanceNormalization()(d)
-        return d
-    def tconv2d(layer_input, filters, f_size=4, s=2):
-        l = Conv2DTranspose(filters, kernel_size=f_size, strides=s, padding="same")(layer_input)
-        l = InstanceNormalization(axis=-1)(l)
-        l = LeakyReLU(alpha=0.2)(l)
-        return l
-    def resnet(layer_input, filters):
-        r = Conv2D(filters, kernel_size=(3,3), strides=1, padding='same')(layer_input)
-        r = LeakyReLU(alpha=0.2)(r)
-        r = Conv2D(layer_input.shape[-1], kernel_size=(3,3), strides=1, padding='same')(r)
-        r = Add()([layer_input, r])
-        r = LeakyReLU(alpha=0.2)(r)
-        r = InstanceNormalization()(r)
-        return r
-    def resnet3(layer_input, filters):
-        for _ in range(3):
-            layer_input = resnet(layer_input, filters)
-        return layer_input
-    def downsampling(layer_input, filters, s=2):
-        d = conv2d(layer_input, filters, s=s)
-        d = resnet3(d, filters)
-        return d
-    def upsampling(layer_input, skip_layer, filters):
-        u = tconv2d(layer_input, skip_layer.shape[-1])
-        u = Add()([skip_layer, u])
-        u = resnet3(u,filters)
-        return u
-    #Input
-    input = Input(shape=IMG_SHAPE)
-    d0 = conv2d(input, GF, s=1)
-    #Downsampling
-    d1 = downsampling(d0, GF*2)
-    d2 = downsampling(d1, GF*4)
-    d3 = downsampling(d2, GF*8)
-    #Upsampling
-    u1 = upsampling(d3, d2, GF*4)
-    u2 = upsampling(u1, d1, GF*2)
-    u3 = tconv2d(u2, GF)
-    output = Conv2D(CHANNELS, kernel_size=4, strides=1, padding='same', activation='tanh')(u3)
-
-    return Model(input, output)
-
-def build_generator_improved_own():
-    """U-Net Generator"""
-
-    def conv2d(layer_input, filters, f_size=4, drate=1):
-        """Layers used during downsampling"""
-        d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
-        if drate > 1:
-            d = Conv2D(filters, kernel_size=f_size, strides=1, dilation_rate=drate, padding='same')(d)
-        d = LeakyReLU(alpha=0.2)(d)
-        d = InstanceNormalization()(d)
-        return d
-
-    def deconv2d(layer_input, skip_input, filters, f_size=4, drate=1, dropout_rate=0):
-        """Layers used during upsampling"""
-        u = UpSampling2D(size=2)(layer_input)
-        if (drate > 1):
-            u = Conv2D(filters, kernel_size=f_size, strides=1, dilation_rate=drate, padding='same')(u)
-            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
-        else:
-            u = Conv2D(filters, kernel_size=f_size, strides=1, activation='relu', padding='same')(u)
-        if dropout_rate:
-            u = Dropout(dropout_rate)(u)
-        u = InstanceNormalization()(u)
-        u = Concatenate()([u, skip_input])
-        return u
-
-    # Image input
-    d0 = Input(shape=IMG_SHAPE)
-
-    # Downsampling
-    d1 = conv2d(d0, GF)
-    d2 = conv2d(d1, GF*2, drate=2)
-    d3 = conv2d(d2, GF*4, drate=4)
-    d4 = conv2d(d3, GF*8, drate=4)
-
-    # Upsampling
-    u1 = deconv2d(d4, d3, GF*4, drate=4)
-    u2 = deconv2d(u1, d2, GF*2, drate=2)
-    u3 = deconv2d(u2, d1, GF)
-
-    u4 = UpSampling2D(size=2)(u3)
-    output_img = Conv2D(CHANNELS, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
-
-    return Model(d0, output_img)
 
 def build_generator():
     """U-Net Generator"""
@@ -256,12 +136,12 @@ g_BA = build_generator()
 #Load
 def load():
     """Sauvegarde les poids deja calculés, pour pouvoir reprendre les calculs plus tard si jamais"""
-    if (os.path.isfile("Weights/d_A.h5") and os.path.isfile("Weights/g_AB.h5") 
-    and os.path.isfile("Weights/d_B.h5.") and os.path.isfile("Weights/g_BA.h5")):
-        d_A.load_weights("Weights/d_A.h5")
-        d_B.load_weights("Weights/d_B.h5")
-        g_AB.load_weights("Weights/g_AB.h5")
-        g_BA.load_weights("Weights/g_BA.h5")
+    if (os.path.isfile("Weights/{}/d_A.h5".format(dataset_name)) and os.path.isfile("Weights/{}/g_AB.h5".format(dataset_name)) 
+    and os.path.isfile("Weights/{}/d_B.h5.".format(dataset_name)) and os.path.isfile("Weights/{}/g_BA.h5".format(dataset_name))):
+        d_A.load_weights("Weights/{}/d_A.h5".format(dataset_name))
+        d_B.load_weights("Weights/{}/d_B.h5".format(dataset_name))
+        g_AB.load_weights("Weights/{}/g_AB.h5".format(dataset_name))
+        g_BA.load_weights("Weights/{}/g_BA.h5".format(dataset_name))
         print("Weights loaded")
     else:
         print("Missing weights files detected. Starting from scratch")
@@ -349,10 +229,10 @@ def sample_images(epoch, batch_i, gif=False):
     plt.close()
 def save():
     """Sauvegarde les poids deja calculés, pour pouvoir reprendre les calculs plus tard si jamais"""
-    d_A.save_weights("Weights/d_A.h5")
-    d_B.save_weights("Weights/d_B.h5")
-    g_AB.save_weights("Weights/g_AB.h5")
-    g_BA.save_weights("Weights/g_BA.h5")
+    d_A.save_weights("Weights/{}/d_A.h5".format(dataset_name))
+    d_B.save_weights("Weights/{}/d_B.h5".format(dataset_name))
+    g_AB.save_weights("Weights/{}/g_AB.h5".format(dataset_name))
+    g_BA.save_weights("Weights/{}/g_BA.h5".format(dataset_name))
 
 start_time = datetime.datetime.now()
 
