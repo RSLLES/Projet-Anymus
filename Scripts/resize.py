3f7348ad360051856f891eaf3b52b32719c83ea6
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-17 12:26:40 +0200

    add exception

--- Scripts/resize.py
+++ Scripts/resize.py
@@ -6,24 +6,28 @@ i = 0
 size = (1024,1024)
 
 for f in glob.glob('*.jpg'):
-    im = Image.open(f)
-    w,h = im.size
+    try:
+        im = Image.open(f)
+        w,h = im.size
 
-    top, bottom = h//2 -1, h//2 + 1
-    left, right = w//2 - 1, w//2 + 1
+        top, bottom = h//2 -1, h//2 + 1
+        left, right = w//2 - 1, w//2 + 1
 
-    #On augmente le carré tant que l'on peut
-    while top > 0 and bottom < h and left > 0 and right < w:
-        top -=1
-        bottom += 1
-        left -= 1
-        right += 1
+        #On augmente le carré tant que l'on peut
+        while top > 0 and bottom < h and left > 0 and right < w:
+            top -=1
+            bottom += 1
+            left -= 1
+            right += 1
 
-    #On crope
-    im = im.crop((left, top, right, bottom)) 
+        #On crope
+        im = im.crop((left, top, right, bottom)) 
 
-    #On save
-    im.thumbnail(size, Image.ANTIALIAS)
-    im.save(f, quality=95)
-    i+=1
-    print("[{}]Done {}".format(i,f))
+        #On save
+        im.thumbnail(size, Image.ANTIALIAS)
+        im.save(f, quality=95)
+        i+=1
+        print("[{}]Done {}".format(i,f))
+    except:
+        print("Erreur de traitement de {}".format(f))
+        os.remove(f)
