b0c5fd3881200483b8027fa6b9dd8d0f4381eca3
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-06-22 15:34:07 +0200

    Add verbose

--- CGAN/run.py
+++ CGAN/run.py
@@ -1,49 +1,5 @@
-import glob
-import os
-import numpy as np
-from tqdm import tqdm
-from PIL import Image
-from MightyMosaic import MightyMosaic
-import argparse
-
-from keras_preprocessing.image import load_img
-from keras_preprocessing.image import img_to_array
-
-from reseaux import build_generator
-from utils import Load_G_AB
-
-
 IMG_SHAPE = (256,256,3)
 OVERLAP_FACTOR = 4
-
-def main():
-    parser = argparse.ArgumentParser(description='Permet de faire tourner le réseau de conversion Face -> Manga sur des images. ')
-    parser.add_argument('image', help="Chemin vers l'image à traiter. Cela peut aussi etre le chemin vers un dossier et dans ce cas toutes les images trouvée dedans seront traitées")
-    parser.add_argument("-o", default="results/", help="Dossier dans lequel seront stockées les résultats. Par défaut 'results/'")
-    parser.add_argument("-g", default="g.h5", help="Chemin vers le fichier generateur.h5 à utiliser. Par défaut 'g.h5'")
-    parser.add_argument("-p", default="result", help="Ajoute le prefixe prefixe-nom_de_l'image_original.jpg au résultat. Aucun par défaut.")
-    parser.add_argument("-m", "--mosaic", action="store_true", help="Utile pour traiter des images hd qui ne doivent pas être redimensionnées en 256x256. Elles seront alors composés d'une mosaic de carrés de tailles 256x256.")
-    parser.add_argument("-v", "--verbose", help="Affiche beaucoup d'information sur la progression", action="store_true")
-    args = parser.parse_args()
-
-    #On vérifie ce que l'on trouve sur le chemin donné
-    isFile,isDirectory = os.path.isfile(args.image), os.path.isdir(args.image)
-
-    if not isFile and not isDirectory:
-        print(f"ERREUR : Le path '{args.image}' ne mene nul part.'")
-        return
-    
-    if isFile and not args.mosaic:
-        run_goodsize(args.image, args.g, args.p, args.o)
-
-    if isFile and args.mosaic:
-        run_img_bigger(args.image, args.g, args.p, args.o)
-
-    if isDirectory and not args.mosaic:
-        run_folder_goodsize()
-
-    if isDirectory and args.mosaic:
-        run_all_folder_bigger()
 
 def load_model(path):
     g,_,_ = build_generator(IMG_SHAPE, name="")
@@ -192,4 +148,51 @@ def run_all_folder_bigger(input_folder, path_to_gen, preffix, results_folder):
 
 
 if __name__ == "__main__":
-    main()
+    import os
+    import argparse
+    parser = argparse.ArgumentParser(description='Permet de faire tourner le réseau de transformation Visage -> Manga sur des images. ')
+    parser.add_argument('image', help="Chemin vers l'image à traiter. Cela peut aussi etre un chemin vers un dossier, auquel cas toutes les images trouvées dedans seront traitées")
+    parser.add_argument("-o", default="results/", help="Dossier dans lequel seront stockés les résultats. Par défaut 'results/'")
+    parser.add_argument("-g", default="g.h5", help="Chemin vers le fichier contenant les poids du générateur à utiliser. Par défaut 'g.h5'")
+    parser.add_argument("-p", default="result", help="Ajoute le prefixe prefixe-nom_de_l'image_original.jpg au résultat. 'result' par défaut.")
+    parser.add_argument("-m", "--mosaic", action="store_true", help="Pour traiter des images hd qui ne doivent pas être redimensionnées en 256x256. Elles seront alors composées d'une mosaic de carrés de tailles 256x256.")
+    parser.add_argument("-v", "--verbose", help="Affiche les logs de tensorflow", action="store_true")
+    args = parser.parse_args()
+
+    #Verbose
+    if args.verbose:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
+    else:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    #Importation
+    import glob
+    import numpy as np
+    from tqdm import tqdm
+    from PIL import Image
+    from MightyMosaic import MightyMosaic
+
+    from keras_preprocessing.image import load_img
+    from keras_preprocessing.image import img_to_array
+
+    from reseaux import build_generator
+    from utils import Load_G_AB
+
+
+    #On vérifie ce que l'on trouve sur le chemin donné
+    isFile,isDirectory = os.path.isfile(args.image), os.path.isdir(args.image)
+
+    if not isFile and not isDirectory:
+        print(f"ERREUR : Le path '{args.image}' ne mene nul part.'")
+    else:
+        if isFile and not args.mosaic:
+            run_goodsize(args.image, args.g, args.p, args.o)
+
+        if isFile and args.mosaic:
+            run_img_bigger(args.image, args.g, args.p, args.o)
+
+        if isDirectory and not args.mosaic:
+            run_folder_goodsize(args.image, args.g, args.p, args.o)
+
+        if isDirectory and args.mosaic:
+            run_all_folder_bigger(args.image, args.g, args.p, args.o)
